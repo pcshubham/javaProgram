0b939b3eac15db23524e7dbe7a83b53b9a52620a
pcshubham41 <pcshubham41@gmail.com>
2023-01-25 12:37:55 +0530

    first commit

---
+++ src/main/java/oddEvenTestProject/OddEvenNumber.java
@@ -0,0 +1,19 @@
+package oddEvenTestProject;
+
+import java.util.Scanner;
+
+public class OddEvenNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int number = scan.nextInt();
+		System.out.println(checkTheNumberIsEven(number));
+	}
+
+	public static boolean checkTheNumberIsEven(int number) {
+		return number%2==0;
+	}
+
+}
